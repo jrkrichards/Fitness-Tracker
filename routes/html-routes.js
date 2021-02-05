0a9843a7abac6584bb1fe20ba2e3f85720024a83
Jayrich <jrkrichards@gmail.com>
2021-02-05 05:02:04 -0800

    Update routes: created HTML routes to render the correct html to the front-end

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,10 +4,18 @@ const router = express.Router();
 const path = require("path");
 
 // Requiring models to query database
-const db = require("../model");
+const db = require("../model/fitness");
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 module.exports = router;
