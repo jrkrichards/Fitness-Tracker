5c90d8aae178ba30d97745ed4a6bdca44bca430d
Jayrich <jrkrichards@gmail.com>
2021-02-09 06:14:07 -0800

    setting up seeder file to input entries in the db if none exist.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,140 +1,30 @@
 // Setting up variables
 const express = require("express");
 const mongojs = require("mongojs");
+const seed = require("../seeders/seed")
 const router = express.Router();
 
 // Requiring model to query database
 const db = require("../model/fitness");
-
-// Setting up seed data
-let workoutSeed = [
-    {
-      day: new Date().setDate(new Date().getDate()-10),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bicep Curl",
-          duration: 20,
-          weight: 100,
-          reps: 10,
-          sets: 4
+const seedDb = {
+    async checkDb() {
+        const itemResult = await db.find({}).exec();
+        if(itemResult.length === 0) {
+            console.log("empty database")
+            db.deleteMany({})
+                .then(() => db.collection.insertMany(seed))
+                .then(data => {
+                    console.log(data.result.n + " records inserted!");
+                })
+                .catch(err => {
+                console.error(err);
+                });
+        } else {
+            console.log("database has entries")
         }
-      ]
-    },
-    {
-      day: new Date().setDate(new Date().getDate()-9),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Lateral Pull",
-          duration: 20,
-          weight: 300,
-          reps: 10,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date().setDate(new Date().getDate()-8),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Push Press",
-          duration: 25,
-          weight: 185,
-          reps: 8,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date().setDate(new Date().getDate()-7),
-      exercises: [
-        {
-          type: "cardio",
-          name: "Running",
-          duration: 25,
-          distance: 4
-        }
-      ]
-    },
-    {
-      day: new Date().setDate(new Date().getDate()-6),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bench Press",
-          duration: 20,
-          weight: 285,
-          reps: 10,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date().setDate(new Date().getDate()-5),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bench Press",
-          duration: 20,
-          weight: 300,
-          reps: 10,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date(new Date().setDate(new Date().getDate() - 4)),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Quad Press",
-          duration: 30,
-          weight: 300,
-          reps: 10,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date(new Date().setDate(new Date().getDate() - 3)),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Bench Press",
-          duration: 20,
-          weight: 300,
-          reps: 10,
-          sets: 4
-        }
-      ]
-    },
-    {
-      day: new Date(new Date().setDate(new Date().getDate() - 2)),
-      exercises: [
-        {
-          type: "resistance",
-          name: "Military Press",
-          duration: 20,
-          weight: 300,
-          reps: 10,
-          sets: 4
-        }
-      ]
     }
-  ];
-  
-//   db.deleteMany({})
-//     .then(() => db.collection.insertMany(workoutSeed))
-//     .then(data => {
-//       console.log(data.result.n + " records inserted!");
-//       process.exit(0);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       process.exit(1);
-//     });
+}
+seedDb.checkDb()
 
 router.get('/api/workouts', (req, res) => {
     db.aggregate([
